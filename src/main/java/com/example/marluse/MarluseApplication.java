1aaee4096d44922efc32dfb50b16b13678be53cc
Miguel Lopes <mpmiguel2007@gmail.com>
2026-06-10 17:21:14 -0300

    feat: Commit de inicialização

---
+++ src/main/java/com/example/marluse/MarluseApplication.java
@@ -0,0 +1,13 @@
+package com.example.marluse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarluseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarluseApplication.class, args);
+	}
+
+}
