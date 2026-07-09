f3604956eafc1aa32d8909146a18eaf7e61f7ce2
Miguel <mpmiguel2007@gmail.com>
2026-07-09 19:51:38 -0300

    fix: Ajuste de permisão de requisão

--- src/main/java/com/example/marluse/security/SecurityConfig.java
+++ src/main/java/com/example/marluse/security/SecurityConfig.java
@@ -75,4 +75,11 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager()
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
