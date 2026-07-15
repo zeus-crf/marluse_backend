8547af60e22dfb4a66613a48f51f82238b5707bc
Miguel <mpmiguel2007@gmail.com>
2026-07-15 18:22:40 -0300

    feat: Novos campos em ItemLocacão e ajuste na migrations V6

--- src/main/resources/db/migration/V6__add_controle_estoque_por_item.sql
+++ src/main/resources/db/migration/V6__add_controle_estoque_por_item.sql
@@ -7,3 +7,11 @@ ALTER TABLE itens_locacao
     ADD COLUMN baixar_estoque        TINYINT(1) NOT NULL DEFAULT 1,
     ADD COLUMN permitir_sem_estoque  TINYINT(1) NOT NULL DEFAULT 0,
     ADD COLUMN estoque_descontado    TINYINT(1) NOT NULL DEFAULT 0;
+
+UPDATE itens_pedido i
+    JOIN pedidos p ON p.id = i.pedido_id
+    SET i.estoque_descontado = p.estoque_descontado;
+
+UPDATE itens_locacao i
+    JOIN locacoes l ON l.id = i.locacao_id
+    SET i.estoque_descontado = l.estoque_descontado;
