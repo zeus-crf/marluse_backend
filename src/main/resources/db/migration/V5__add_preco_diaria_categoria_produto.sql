f36edf4f415664c611ac1e6f6b7a7d34bfcea2e6
Miguel <mpmiguel2007@gmail.com>
2026-07-15 17:11:39 -0300

    fix: Ajuste nas migrations

--- src/main/resources/db/migration/V5__add_preco_diaria_categoria_produto.sql
+++ src/main/resources/db/migration/V5__add_preco_diaria_categoria_produto.sql
@@ -1,6 +1,6 @@
 ALTER TABLE produtos
     ADD COLUMN preco_diaria DECIMAL(10, 2) NULL,
-    ADD COLUMN categoria VARCHAR(50) NULL;
+    ADD COLUMN categoria VARCHAR(30) NULL;
 
 
 UPDATE produtos SET preco_diaria = preco WHERE preco_diaria IS NULL;
@@ -8,4 +8,4 @@ UPDATE produtos SET categoria = 'OUTROS' WHERE categoria IS NULL;
 
 ALTER TABLE produtos
     MODIFY COLUMN preco_diaria DECIMAL(10,2) NOT NULL,
-    MODIFY COLUMN categoria    VARCHAR(30)   NOT NULL;dd
+    MODIFY COLUMN categoria    VARCHAR(30)   NOT NULL;
